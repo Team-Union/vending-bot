0126e5c2bfa43a74207e599e906ee4409aae2a6c
Tim232 <64291996+Tim232@users.noreply.github.com>
2020-11-21 00:10:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import traceback
 import json
 
 import discord
 from discord import HTTPException
 from discord.ext import commands
+from discord.ext.commands import errors
 
-with open("table.json", "r", encoding="utf-8") as f:
+with open("db.json", "r", encoding="utf-8") as f:
     jpgtb = json.load(f)
 
 bot = commands.Bot(command_prefix="!자판기 ")
+bot.remove_command('help')
+
+# configuration
+version = "v.0.1.4"
+token = "NzMzMjUzMDY4OTQ2Mjc2Mzkz.XxAdIg.ReKI01-xImzzhxHFTHO4lacBubU"
 
 
 @bot.event
@@ -25,6 +32,7 @@ async def on_command(ctx):
     except KeyError:
         jpgtb[str(ctx.guild.id)] = [[]]
     except HTTPException:
+        traceback.print_exc()
         sjt = []
         try:
             sjt = jpgtb[str(ctx.guild.id)]
@@ -35,10 +43,9 @@ async def on_command(ctx):
             for j in i:
                 f.append(j)
         goza = {
-            "index": str(len(f) + 1),
-            "name": "등록된 물건 없음",
-            "price": "2147483647",
-            "description": "Wild MISSINGNO. appeared!",
+            "name": "등록된 상품 없음",
+            "price": "등록된 가격 없음",
+            "description": "등록된 설명 없음",
         }
         var = []
         try:
@@ -51,7 +58,7 @@ async def on_command(ctx):
 
 @bot.after_invoke
 async def after(ctx):
-    with open("table.json", "w") as f:
+    with open("db.json", "w", encoding="utf-8") as f:
         json.dump(jpgtb, f, indent=4)
 
 
@@ -61,7 +68,7 @@ async def hellothisisverification(ctx):
 
 
 @bot.command()
-async def 물건보기(ctx, pid: int = None):
+async def 상품보기(ctx, pid: int = None):
     if pid is not None:
         sjt = []
         try:
@@ -74,7 +81,7 @@ async def 물건보기(ctx, pid: int = None):
         lsd = sjt[pid - 1]
         em = discord.Embed(
             title=f"전체 품목 중 {pid} 페이지 ({15 * (pid - 1) + 1} - {15 * (pid - 1) + len(lsd)})",
-            description="성공 - 물건 목록 보기",
+            description="성공 - 상품 목록 보기",
             color=0x00FF00,
         )
         efo = []
@@ -86,10 +93,10 @@ async def 물건보기(ctx, pid: int = None):
         efp = []
         [efp.append(x["price"]) for x in lsd]
         eps = "\n".join(efp)
-        em.add_field(name="물건 번호", value=eos, inline=True)
-        em.add_field(name="물건 이름", value=ens, inline=True)
-        em.add_field(name="물건 가격", value=eps, inline=True)
-        em.set_footer(text="Vending Bot v0.1.3")
+        em.add_field(name="상품 번호", value=eos, inline=True)
+        em.add_field(name="상품 이름", value=ens, inline=True)
+        em.add_field(name="상품 가격", value=eps, inline=True)
+        em.set_footer(text=f"Vending Bot {version}")
         await ctx.send(embed=em)
     else:
         sjt = []
@@ -104,7 +111,7 @@ async def 물건보기(ctx, pid: int = None):
         lsd = sjt[pid - 1]
         em = discord.Embed(
             title=f"전체 품목 중 1 페이지 (1 - {15 * (pid - 1) + len(lsd)})",
-            description="성공 - 물건 목록 보기",
+            description="성공 - 상품 목록 보기",
             color=0x00FF00,
         )
         efo = []
@@ -116,14 +123,14 @@ async def 물건보기(ctx, pid: int = None):
         efp = []
         [efp.append(x["price"]) for x in lsd]
         eps = "\n".join(efp)
-        em.add_field(name="물건 번호", value=eos, inline=True)
-        em.add_field(name="물건 이름", value=ens, inline=True)
-        em.add_field(name="물건 가격", value=eps, inline=True)
-        em.set_footer(text="Vending Bot v0.1.3")
+        em.add_field(name="상품 번호", value=eos, inline=True)
+        em.add_field(name="상품 이름", value=ens, inline=True)
+        em.add_field(name="상품 가격", value=eps, inline=True)
+        em.set_footer(text=f"Vending Bot {version}")
         await ctx.send(embed=em)
 
 
-@bot.command()
+@bot.command(aliases=("help",))
 async def 도움(ctx):
     em = discord.Embed(
         title=f"자판기봇 도움말",
@@ -131,27 +138,27 @@ async def 도움(ctx):
         color=0x00FF00,
     )
     eos = """
-    !자판기 물건보기 (페이지)
-    !자판기 물건설명 번호
-    !자판기 물건등록 이름 가격 설명
-    !자판기 물건삭제 번호
+    !자판기 상품보기 (페이지)
+    !자판기 상품설명 번호
+    !자판기 상품등록 이름 가격 설명
+    !자판기 상품삭제 번호
     """
     ens = """
-    (페이지)번째 페이지의 물건 목록을 봅니다.
-    번호에 해당하는 물건의 설명을 봅니다.
-    이름, 가격, 설명을 가진 물건을 등록합니다.
-    번호에 해당하는 물건을 삭제합니다.
+    (페이지)번째 페이지의 상품 목록을 봅니다.
+    번호에 해당하는 상품의 설명을 봅니다.
+    이름, 가격, 설명을 가진 상품을 등록합니다.
+    번호에 해당하는 상품을 삭제합니다.
     """
     em.add_field(name="명령어", value=eos, inline=True)
     em.add_field(name="설명", value=ens, inline=True)
     em.add_field(name="개발자", value="! Tim23#9999, \nDeleted User 00000000#2214",
                  inline=False)
-    em.set_footer(text="Vending Bot v0.1.3")
+    em.set_footer(text=f"Vending Bot {version}")
     await ctx.send(embed=em)
 
 
 @bot.command()
-async def 물건설명(ctx, n: int):
+async def 상품설명(ctx, n: int):
     sjt = jpgtb[str(ctx.guild.id)]
     f = []
     for i in sjt:
@@ -164,20 +171,20 @@ async def 물건설명(ctx, n: int):
     if not f:
         f = [{
             "index": str(len(f) + 1),
-            "name": "등록된 물건 없음",
-            "price": "2147483647",
-            "description": "Wild MISSINGNO. appeared!",
+            "name": "등록된 상품 없음",
+            "price": "등록된 가격 없음",
+            "description": "등록된 설명 없음",
         }]
     k = f[n - 1]
-    em = discord.Embed(title=f"{n}번 물건의 설명", description="", color=0x00FF00)
+    em = discord.Embed(title=f"{n}번 상품의 설명", description="", color=0x00FF00)
     em.add_field(name=k["name"], value=k["description"], inline=True)
-    em.set_footer(text="Vending Bot v0.1.3")
+    em.set_footer(text=f"Vending Bot {version}")
     await ctx.send(embed=em)
 
 
 @bot.command()
 @commands.has_permissions(administrator=True)
-async def 물건등록(ctx, name, price, *, description):
+async def 상품등록(ctx, name, price, *, description):
     sjt = jpgtb[str(ctx.guild.id)]
     f = []
     for i in sjt:
@@ -198,34 +205,37 @@ async def 물건등록(ctx, name, price, *, description):
         var = sjt[int(len(f) / 15)]
     var.append(goza)
     em = discord.Embed(title=f"{name}(이)가 생성됨", description="", color=0x00FF00)
-    em.add_field(name="물건 번호", value=str(len(f) + 1), inline=True)
-    em.add_field(name="물건 이름", value=name, inline=True)
-    em.add_field(name="물건 가격", value=price, inline=True)
-    em.set_footer(text="Vending Bot v0.1.3")
+    em.add_field(name="상품 번호", value=str(len(f) + 1), inline=True)
+    em.add_field(name="상품 이름", value=name, inline=True)
+    em.add_field(name="상품 가격", value=price, inline=True)
+    em.set_footer(text=f"Vending Bot {version}")
     await ctx.send(embed=em)
 
 
 @bot.command()
 @commands.has_permissions(administrator=True)
-async def 물건삭제(ctx, v: int):
+async def 상품삭제(ctx, v: int):
     sjt = jpgtb[str(ctx.guild.id)]
     var = sjt[int(v / 15)]
     print(str((v - int(v / 15) * 15)))
     print(str(var))
     del var[(v - (int(v / 15) * 15)) - 1]
-    em = discord.Embed(title=f"{v}번 물건이 삭제됨", description="삭제가 완료되었습니다.", color=0xFF00)
-    em.set_footer(text="Vending Bot v0.1.3")
+    em = discord.Embed(title=f"{v}번 상품이 삭제됨", description="삭제가 완료되었습니다.", color=0xFF00)
+    em.set_footer(text=f"Vending Bot {version}")
     await ctx.send(embed=em)
 
 
 @bot.event
 async def on_command_error(ctx, err):
-    if isinstance(err, commands.errors.MissingPermissions):
-        em = discord.Embed(title="오류", description="당신은 이 커맨드를 사용할 권한이 없습니다!", color=0xFF0000)
-        em.set_footer(text="Vending Bot v0.1.3")
+    if isinstance(err, errors.MissingPermissions):
+        em = discord.Embed(title="오류 : 권한 부족", description="당신은 이 커맨드를 사용할 권한이 없습니다!", color=0xFF0000)
+        em.set_footer(text=f"Vending Bot {version}")
         return await ctx.send(embed=em)
+    
+    elif isinstance(err, errors.CommandNotFound):
+        return
 
     raise err
 
 
-# bot.run()
+bot.run(token)
